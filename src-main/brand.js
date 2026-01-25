6a013616fd8890829ab1b9dbaa525f05ab99ea5a
lgm <2663253979@qq.com>
2026-01-25 18:39:25 +0800

    修改了版本号0.2.14

--- src-main/brand.js
+++ src-main/brand.js
@@ -1,9 +1,11 @@
-const PLATFORM_NAME = 'ICreateCode Beta 0.2.9';
-const APP_NAME = 'ICreateCode Beta 0.2.9';
+const PLATFORM_NAME = 'ICreateCode Beta 0.2.14';
+const APP_NAME = 'ICreateCode Beta 0.2.14';
+const APP_VERSION = '0.2.14';
 const PACKAGER_NAME = 'TurboWarp Packager';
 
 module.exports = {
   PLATFORM_NAME,
   APP_NAME,
-  PACKAGER_NAME
+  PACKAGER_NAME,
+  APP_VERSION
 };
