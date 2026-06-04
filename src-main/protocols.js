a384ce68138d5d0b8765f8a0307257731923ab1a
lgm <2663253979@qq.com>
2026-06-04 16:50:33 +0800

    新增了允许访问的文件后缀

--- src-main/protocols.js
+++ src-main/protocols.js
@@ -120,6 +120,8 @@ MIME_TYPES.set('.css', 'text/css');
 MIME_TYPES.set('', 'application/octet-stream');
 MIME_TYPES.set('.tflite', 'application/octet-stream');
 MIME_TYPES.set('.sb3', 'application/octet-stream');
+MIME_TYPES.set('.data', 'application/octet-stream');
+MIME_TYPES.set('.binarypb', 'application/octet-stream');
 
 protocol.registerSchemesAsPrivileged(Object.entries(FILE_SCHEMES).map(([scheme, metadata]) => ({
   scheme,
